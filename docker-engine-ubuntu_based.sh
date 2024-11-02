4439f9def85ad82476a930079be8887d87ae332c
Berkan Deniz Sahiner <161295930+bdsahiner@users.noreply.github.com>
2024-11-02 23:33:07 +0200

    Update docker-engine-ubuntu_based.sh

--- docker-engine-ubuntu_based.sh
+++ docker-engine-ubuntu_based.sh
@@ -9,8 +9,12 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker || true
+sudo usermod -aG docker $(whoami)
+newgrp docker
 
 sudo docker --version
+docker run hello-world
